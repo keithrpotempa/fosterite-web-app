922fdca5e29be4efdfc666f4143ac4b837b992d1
Keith Potempa <keithrpotempa@gmail.com>
2020-06-11 11:07:36 -0500

    installed material ui, added catcard component

--- src/pages/cats/Cats.js
+++ src/pages/cats/Cats.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { catManager } from "../../modules" 
+import { CatCard } from "../../components"
 
 const Cats = props => {
+  const [catList, setCatList] = useState([]);
+
+  const getCatList = () => {
+    catManager.getCatList()
+      .then(setCatList)
+  }
+
+  useEffect(() => {
+    getCatList()
+  }, [])
+
   return (
     <>
       <h1>Cats Page</h1>
+      {catList.map( cat => (
+        <CatCard
+          key={cat.id}
+          cat={cat}
+        />
+      ))}
     </>
   )
 }
